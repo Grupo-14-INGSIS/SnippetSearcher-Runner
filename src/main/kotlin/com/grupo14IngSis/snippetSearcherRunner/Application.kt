1249d1c44af3ad0baafa61960694049d9d03eb6c
SergioMorero <smorero@mail.austral.edu.ar>
2025-12-16 16:28:52 -0300

    .

--- src/main/kotlin/com/grupo14IngSis/snippetSearcherRunner/Application.kt
+++ src/main/kotlin/com/grupo14IngSis/snippetSearcherRunner/Application.kt
@@ -2,8 +2,10 @@ package com.grupo14IngSis.snippetSearcherRunner
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["com.grupo14IngSis.snippetSearcherRunner", "com.grupo14IngSis.snippetSearcherRunner.plugins"])
 class Application
 
 fun main(args: Array<String>) {
